ad06c6fde48546a31af707c321802c82c573b54b
nurtai325 <140373832+nurtai325@users.noreply.github.com>
2024-02-13 20:02:45 +0600

    .

--- src/ui/SideButton.jsx
+++ src/ui/SideButton.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 export default function SideButton(){
     return (
-       <Link href='/create'>
+       <Link to='/create'>
         <div className="w-[40px] h-[40px] bg-transparent mt-[1px] pt-2 pl-[7.5px] rounded-full hover:bg-button">
             <IoIosAddCircleOutline color="grey" size={25}/>
         </div>
